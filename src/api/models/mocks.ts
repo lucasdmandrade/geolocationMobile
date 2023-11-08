797e985faab399486670164a748412e4be716b85
Lucas Andrade <lucasdmandrade@gmail.com>
2023-11-07 21:35:37 -0300

    mock

---
+++ src/api/models/mocks.ts
@@ -0,0 +1,39 @@
+import { AllPackagesKeys, LocationPoint } from ".";
+
+export const mockPoints: { points: LocationPoint }[] = [
+  {
+    points: {
+      id: "bd6c780f-be9a-453f-9eb3-2d5a9c8f84cb",
+      latitude: 37.4219983,
+      longitude: -122.084,
+      speed: 0,
+      time: "2023-11-06T19:02:34.848Z",
+    },
+  },
+  {
+    points: {
+      id: "968af977-7635-4d11-9b99-e0d405cefc24",
+      latitude: 37.4219983,
+      longitude: -122.084,
+      speed: 0,
+      time: "2023-11-06T18:55:35.981Z",
+    },
+  },
+  {
+    points: {
+      id: "addd45ff-5df5-4534-a147-93932e4721c2",
+      latitude: 37.4219983,
+      longitude: -122.084,
+      speed: 0,
+      time: "2023-11-06T19:09:45.016Z",
+    },
+  },
+];
+
+export const pointsKeys: AllPackagesKeys = {
+  keys: [
+    "bd6c780f-be9a-453f-9eb3-2d5a9c8f84cb",
+    "968af977-7635-4d11-9b99-e0d405cefc24",
+    "addd45ff-5df5-4534-a147-93932e4721c2",
+  ],
+};
